6808ffa2458dd55fe91a1ff514792012a8146540
ax003d <ax003d@gmail.com>
2013-04-17 00:47:29 +0800

    * generalize a class

--- getui.py
+++ getui.py
@@ -8,11 +8,7 @@ __author__  = 'ax003d@gmail.com'
 Python SDK for getui server API.
 
 Usage:
-1. Replace APPID, APPKEY, MASTERSECRET for your own values
-2. Set test_client in last lines for testing
-3. run:
    python getui.py
-   The console will show JSON response from getui.
 '''
 
 import json
@@ -22,63 +18,71 @@ import urllib2
 
 from hashlib import md5
 
-APPID = ''
-APPKEY = ''
-MASTERSECRET = ''
-APIURL = 'http://sdk.open.api.igexin.com/service'
 
 PUSH_TYPE_TRANSMISSION = 'TransmissionMsg' # Not supported yet
 PUSH_TYPE_LINK         = 'LinkMsg'
 PUSH_TYPE_NOTIFY       = 'NotifyMsg'
 
 
-def push_notification(title, message, push_type, subscribers,
-                      link=None):
-    title = title.encode('utf-8')
-    message = message.encode('utf-8')
-    params = {}
-    params['appkey'] = APPKEY
-    params['pushTitle'] = title
-    params['pushType'] = push_type
-    params['tokenMD5List'] = subscribers
+class GXPushClient(object):
+    APIURL = 'http://sdk.open.api.igexin.com/service'
 
-    params['type'] = 2
-    params['action'] = 'pushSpecifyMessage'
-    params['offline'] = True
-    params['offlineTime'] = 2
-    params['priority'] = 1
+    def __init__(self, appid='', appkey='', mastersecret=''):
+        self.appid = appid
+        self.appkey = appkey
+        self.mastersecret = mastersecret
 
-    msg = {}
-    if push_type == PUSH_TYPE_LINK:
-        msg['linkMsgIcon'] = 'ic_launcher.png'
-        msg['linkMsgTitle'] = title
-        msg['linkMsgContent'] = message
-        msg['linkMsgUrl'] = link
-    elif push_type == PUSH_TYPE_NOTIFY:
-        msg['notifyMsgIcon'] = 'ic_launcher.png'
-        msg['notifyMsgTitle'] = title
-        msg['notifyMsgContent'] = message
-        msg['transmissionContent'] = ''
-        msg['transmissionType'] = 1
-    params['msg'] = msg
 
-    sign = [MASTERSECRET]
-    for i in sorted(params.keys()):
-        if type(params[i]) in [types.StringType, types.IntType, types.LongType]:
-            sign.append(i)
-            sign.append(str(params[i]))
-    params['sign'] = md5.md5(''.join(sign)).hexdigest()
+    def push_notification(title, message, push_type, subscribers,
+                          link=None):
+        title = title.encode('utf-8')
+        message = message.encode('utf-8')
+        params = {}
+        params['appkey'] = self.appkey
+        params['pushTitle'] = title
+        params['pushType'] = push_type
+        params['tokenMD5List'] = subscribers
 
-    data = json.dumps(params)
-    req = urllib2.Request(APIURL, data)
-    response = urllib2.urlopen(req)
-    print response.read()
+        params['type'] = 2
+        params['action'] = 'pushSpecifyMessage'
+        params['offline'] = True
+        params['offlineTime'] = 2
+        params['priority'] = 1
+
+        msg = {}
+        if push_type == PUSH_TYPE_LINK:
+            msg['linkMsgIcon'] = 'ic_launcher.png'
+            msg['linkMsgTitle'] = title
+            msg['linkMsgContent'] = message
+            msg['linkMsgUrl'] = link
+        elif push_type == PUSH_TYPE_NOTIFY:
+            msg['notifyMsgIcon'] = 'ic_launcher.png'
+            msg['notifyMsgTitle'] = title
+            msg['notifyMsgContent'] = message
+            msg['transmissionContent'] = ''
+            msg['transmissionType'] = 1
+        params['msg'] = msg
+
+        sign = [self.mastersecret]
+        for i in sorted(params.keys()):
+            if type(params[i]) in [types.StringType, types.IntType, types.LongType]:
+                sign.append(i)
+                sign.append(str(params[i]))
+        params['sign'] = md5.md5(''.join(sign)).hexdigest()
+
+        data = json.dumps(params)
+        req = urllib2.Request(APIURL, data)
+        response = urllib2.urlopen(req)
+        print response.read()
     
 
 if __name__=='__main__':
+    gx = GXPushClient(appid='your appid',
+                  appkey='your appkey',
+                  mastersecret='your mastersecret')
     test_client = 'xxx'
-    push_notification('link title', 'link message', 
-                      PUSH_TYPE_LINK, [test_client],
-                      link='http://www.baidu.com')
-    push_notification('notify title', 'notify message', 
-                      PUSH_TYPE_NOTIFY, [test_client])
+    gx.push_notification('link title', 'link message', 
+                         PUSH_TYPE_LINK, [test_client],
+                         link='http://www.baidu.com')
+    gx.push_notification('notify title', 'notify message', 
+                         PUSH_TYPE_NOTIFY, [test_client])
