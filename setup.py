272b18f2066e520f2c220b95235bb9db17b8796c
ax003d <ax003d@gmail.com>
2013-04-17 00:58:50 +0800

    * fix setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="pygetui"
-    version="0.1"
-    author="ax003d"
-    author_email="ax003d@gmail.com"
-    description="3rd party getui push service server side SDK."
+    name="pygetui",
+    version="0.1",
+    author="ax003d",
+    author_email="ax003d@gmail.com",
+    description="3rd party getui push service server side SDK.",
     license="MIT License",
-    keywords="push server"
-    url="http://github.com/ax003d/pygetui"
+    keywords="push server",
+    url="http://github.com/ax003d/pygetui",
     )
